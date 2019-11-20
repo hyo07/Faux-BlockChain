7b227801e6eda904fa83f814506b977b85352b05
hyo07 <yutaka727kato@gmail.com>
2019-11-20 19:59:42 +0900

    CLI上で簡単に動かせるように

--- BlockChain.py
+++ BlockChain.py
@@ -7,13 +7,9 @@ JST = timezone(timedelta(hours=+9), 'JST')
 
 
 class BlockChain:
-    def __init__(self, user_name: str, message: str, nonce: int):
+    def __init__(self):
         self.genesis_block = self.make_genesis()
         self.block_chain = [self.genesis_block]
-
-        self.user_name = user_name
-        self.message = message
-        self.nonce = nonce
 
     def make_genesis(self):
         gene_nonce = 0
@@ -30,22 +26,24 @@ class BlockChain:
             else:
                 gene_nonce += 1
 
-    def get_new_block(self):
+    def get_new_block(self, user_name: str, message: str, nonce: int):
         new_bc = {
             "prev_hash": self.get_hash(json.dumps(self.block_chain[-1])),
             # "time_stamp": datetime.now(JST).strftime("%Y-%m-%d %H:%M:%S.%f"),
             "time_stamp": "なうw",
-            "user_name": self.user_name,
-            "message": self.message,
-            "nonce": self.nonce
+            "user_name": user_name,
+            "message": message,
+            "nonce": nonce
         }
         new_hash = self.get_hash(json.dumps(new_bc))
         if self.judge_hash(new_hash):
             self.block_chain.append(new_bc)
-            print(self.block_chain)
-            return True
+            return True, new_hash
         else:
-            return False
+            return False, new_hash
+
+    def get_current_block(self):
+        return self.block_chain
 
     def get_hash(self, new_data: str):
         return hashlib.sha256(new_data.encode()).hexdigest()
@@ -61,19 +59,41 @@ class BlockChain:
 if __name__ == "__main__":
     pass
 
-    USER_NAME = "yutaka"
-    MESSAGE = "てすと〜"
-    NONCE = 16
+    # USER_NAME = "わたしだ"
+    # MESSAGE = "動く"
+    # NONCE = 35
 
-    # bc = BlockChain(USER_NAME, MESSAGE, NONCE)
-    # print(bc.get_new_block())
+    # bc = BlockChain()
+    # bc.get_new_block(USER_NAME, MESSAGE, NONCE)
+    # print(bc.get_current_block())
 
-    count = 1
+    # count = 1
+    # bc = BlockChain()
+    # while True:
+    #     print(count)
+    #     new_bc, res_hash = bc.get_new_block(USER_NAME, MESSAGE, count)
+    #     print(new_bc)
+    #     if new_bc:
+    #         print(bc.get_current_block())
+    #         break
+    #     else:
+    #         count += 1
+
+    bc = BlockChain()
     while True:
-        print(count)
-        bc = BlockChain(USER_NAME, MESSAGE, count)
-        new_bc = bc.get_new_block()
+        print("↓↓↓↓↓ 現在のブロックチェーン ↓↓↓↓↓")
+        print(json.dumps(bc.get_current_block(), indent=2))
+        print()
+        print("保存情報を入力してください")
+        user_name = input("user_name: ")
+        message = input("message: ")
+        nonce = int(input("nonce: "))
+        new_bc, new_hash = bc.get_new_block(user_name, message, nonce)
+        print("")
+        print("生成ハッシュ値:", new_hash)
         if new_bc:
-            break
+            print("ブロックの生成に成功しました")
         else:
-            count += 1
+            print("ブロックの生成に失敗しました")
+
+        print("---------------------------------------")
